8595208642369c981d8f02dfac348bcf9d35b3e2
tician <tician@gmail.com>
2013-01-15 21:52:46 -0500

    first additions with more to come, might wind up writing yet another SGA implementation to make me feel useful.  Or work on an attempt to use floor/ceiling tiles and/or cinderblocks for visual odometry (been done before, but haven't finished reading those papers yet).

---
+++ sga/fitness.h
@@ -0,0 +1,16 @@
+/*
+ * I was really fucking bored and depressed...
+ */
+
+#include "TendencyMatrix10x25.h"
+
+
+__float128 fitness(uint32_t diagnosis)
+{
+	__float128 L1 = 1.0;
+	__float128 L2 = 1.0;
+	
+
+
+}
+
