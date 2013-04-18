74b882d57218b585fc8aca6c27ec4b149859074e
tician <tician@gmail.com>
2013-04-18 16:03:34 -0400

    compiles, but has seg fault.

--- sga/mfd/mfdga.cpp
+++ sga/mfd/mfdga.cpp
@@ -9,13 +9,10 @@
 
 #include "TendencyMatrix10x25.h"
 
-bool lte_comp (uint32_t i,uint32_t j) { return (i<=j); }
-
-
 
 #define NUMBER_TRIALS				10
 #define NUMBER_GENERATIONS			30
-#define NUMBER_INDIVIDUALS			2000
+#define NUMBER_INDIVIDUALS			200
 #define NUMBER_TRACKING				3
 #define NUMBER_GENES				25
 #define NUMBER_DIMENSIONS			10
@@ -42,131 +39,38 @@ typedef struct
 	GENO_TYPE		gen;
 } specimen_t;
 
-class specimen//: public CPolyg
-{
-private:
-	FITNESS_TYPE fit_;
-	GENO_TYPE gen_;
 
-	void calcFitness(void);
-
-public:
-	void populate(RNG& rudi);
-	void mutate(RNG& rudi);
-	FITNESS_TYPE fitness(void) {return fit_;}
-	GENO_TYPE individual(void) {return gen_;}
-	GENO_TYPE swapper(GENO_TYPE newb) {gen_ = newb; return gen_;}
-	specimen_t puke(void);
-};
-
-specimen_t specimen::puke(void)
-{
-	specimen_t temp;
-	temp.fit = fit_;
-	temp.gen = gen_;
-	return temp;
-}
-
-void specimen::populate(RNG& rudi)
-{
-	gen_ = rudi.uniform(0, 2^(NUMBER_DIMENSIONS));
-	calcFitness();
-}
-
-void specimen::mutate(RNG& rudi)
-{
-	uint32_t iter;
-	for (iter=0; iter<NUMBER_GENES; iter++)
-	{
-		if (rudi.uniform( 0, (int)(1/MUTATION_RATE) ) < 1)
-		{
-			gen_.flip(iter);
-		}
-	}
-	calcFitness();
-}
-
-void specimen::calcFitness(void)
-{
-	FITNESS_TYPE temp = 1.0;
-	FITNESS_TYPE L1 = 1.0;
-	FITNESS_TYPE L2 = 1.0;
-	FITNESS_TYPE L3 = 1.0;
-	uint32_t iter = 0, jter = 0;
-
-	// Positive Likelihood
-	for (iter=0; iter<NUMBER_SYMPTOMS; iter++)
-	{
-		if (SymptomSet[iter])
-		{
-			temp = 1.0;
-			for (jter=0; jter<NUMBER_GENES; jter++)
-			{
-				if (gen_[jter])
-				{
-					temp *= (1.0-qManifestationInDisease[iter][jter]);
-				}
-			}
-			L1 *= (1.0-temp);
-			if ((1.0-temp)>ZERO_FITNESS_LIMIT)
-			{
-				L1 *= (1.0-temp);
-			}
-			else
-			{
-				L1 *= ZERO_FITNESS_LIMIT;
-			}
-		}
-	}
-
-	// Negative Likelihood
-	for (iter=0; iter<NUMBER_GENES; iter++)
-	{
-		if (gen_[iter])
-		{
-			temp = 1.0;
-			for (jter=0; jter<NUMBER_SYMPTOMS; jter++)
-			{
-				if (SymptomSet[jter] == 0)
-				{
-					temp *= (1.0-qManifestationInDisease[jter][iter]);
-				}
-			}
-			L2 *= (temp);
-		}
-	}
-
-	// Prior Likelihood
-	for (iter=0; iter<NUMBER_GENES; iter++)
-	{
-		if (gen_[iter])
-		{
-			L3 *= qPriorLikelihood[iter];
-		}
-	}
-
-	fit_ = (L1 * L2 * L3);
-}
+bool loci_comp (uint32_t i,uint32_t j) { return (i<=j); }
+bool spec_comp (specimen_t i, specimen_t j) { return ((FITNESS_TYPE)i.fit>=(FITNESS_TYPE)j.fit); }
 
 
 class population
 {
 private:
-	specimen pop_[NUMBER_INDIVIDUALS];
-	specimen best_[NUMBER_TRACKING][NUMBER_GENERATIONS];
+	specimen_t pop_[NUMBER_INDIVIDUALS];
+	specimen_t best_[NUMBER_TRACKING][NUMBER_GENERATIONS];
 
 	FITNESS_TYPE sig_fit_[NUMBER_INDIVIDUALS];
 
+	RNG rudi_;
+
 	void roulette(void);
-	void selector(RNG&, GENO_TYPE&);
-	void splicer(RNG&, GENO_TYPE&, GENO_TYPE&);
-	void mutator(RNG&);
+	void selector(GENO_TYPE&);
+	void splicer(GENO_TYPE&, GENO_TYPE&);
+	void mutator(void);
 
 	uint32_t generation_;
+	void bestest(void);
+
+
+	void calcFitness(specimen_t&);
+	specimen_t populate(void);
+	specimen_t mutate(specimen_t);
 
 public:
-	void populator(RNG&);
-	void breeder(RNG&);
+	population(RNG& rudi) {rudi_ = rudi;}
+	void populator(void);
+	void breeder(void);
 
 	specimen_t First (uint32_t);
 	specimen_t Second(uint32_t);
@@ -178,7 +82,7 @@ public:
 specimen_t population::First (uint32_t gen_index)
 {
 	assert(gen_index<NUMBER_GENERATIONS);
-	return best_[0][gen_index].puke();
+	return best_[0][gen_index];
 //	if (gen_index<NUMBER_GENERATIONS)
 //		return best_[0][gen_index];
 //	else
@@ -187,7 +91,7 @@ specimen_t population::First (uint32_t gen_index)
 specimen_t population::Second(uint32_t gen_index)
 {
 	assert(gen_index<NUMBER_GENERATIONS);
-	return best_[1][gen_index].puke();
+	return best_[1][gen_index];
 //	if (gen_index<NUMBER_GENERATIONS)
 //		return best_[1][gen_index];
 //	else
@@ -196,16 +100,46 @@ specimen_t population::Second(uint32_t gen_index)
 specimen_t population::Third (uint32_t gen_index)
 {
 	assert(gen_index<NUMBER_GENERATIONS);
-	return best_[2][gen_index].puke();
+	return best_[2][gen_index];
 //	if (gen_index<NUMBER_GENERATIONS)
 //		return best_[2][gen_index];
 //	else
 //		return NULL;
 }
 
+void population::bestest(void)
+{
+	std::vector<specimen_t> punk (pop_, pop_+NUMBER_INDIVIDUALS);
+	std::sort (punk.begin(), punk.end(), spec_comp);
+
+	std::vector<specimen_t>::iterator it;
+//	it = std::unique(punk.begin(), punk.end());
+//	punk.resize( std::distance(punk.begin(),it) );
+
+//	for (it=loci.begin(); it!=loci.end(); it++)//++it)
+//		std::cout << ' ' << *it;
+
+	it = punk.begin();
+
+	best_[0][generation_] = (*it);
+	do
+	{
+		it++;
+	}
+	while( ((*it).gen == best_[0][generation_].gen) && (it<punk.end()) );
+	best_[1][generation_] = (*it);
+	do
+	{
+		it++;
+	}
+	while( ((*it).gen == best_[1][generation_].gen) && (it<punk.end()) );
+
+	if (it<punk.end())
+		best_[2][generation_] = (*it);
+}
 
 
-void population::populator(RNG& rudi)
+void population::populator(void)
 {
 	generation_ = 0;
 
@@ -213,12 +147,12 @@ void population::populator(RNG& rudi)
 
 	for (iter=0; iter<NUMBER_INDIVIDUALS; iter++)
 	{
-		pop_[iter].populate(rudi);
+		pop_[iter] = populate();
 	}
-
+	bestest();
 }
 
-void population::breeder(RNG& rudi)
+void population::breeder(void)
 {
 	GENO_TYPE kiddies[NUMBER_INDIVIDUALS];
 	roulette();
@@ -228,19 +162,20 @@ void population::breeder(RNG& rudi)
 	{
 		GENO_TYPE mama, papa;
 
-		selector(rudi, mama);
-		selector(rudi, papa);
-		splicer(rudi, mama, papa);
+		selector(mama);
+		selector(papa);
+		splicer(mama, papa);
 		kiddies[(2*iter)+0] = mama;
 		kiddies[(2*iter)+1] = papa;
 	}
 
 	for (iter=0; iter<NUMBER_INDIVIDUALS; iter++)
 	{
-		pop_[iter].swapper(kiddies[iter]);
+		pop_[iter].gen = kiddies[iter];
 	}
-	mutator(rudi);
+	mutator();
 	generation_++;
+	bestest();
 }
 
 void population::roulette(void)
@@ -248,30 +183,30 @@ void population::roulette(void)
 	// Roulette Wheel Selection
 	uint32_t iter;
 
-	sig_fit_[0] = pop_[0].fitness();
+	sig_fit_[0] = pop_[0].fit;
 	for (iter=1; iter<NUMBER_INDIVIDUALS; iter++)
 	{
-		sig_fit_[iter] = sig_fit_[iter-1] + pop_[iter].fitness();
+		sig_fit_[iter] = sig_fit_[iter-1] + pop_[iter].fit;
 	}
 }
 
-void population::selector(RNG& rudi, GENO_TYPE& nana)
+void population::selector(GENO_TYPE& nana)
 {
 	uint32_t iter;
 	// Roulette Wheel Selection
-	FITNESS_TYPE temp = rudi.uniform(0.0, sig_fit_[NUMBER_INDIVIDUALS-1]);
+	FITNESS_TYPE temp = rudi_.uniform(0.0, sig_fit_[NUMBER_INDIVIDUALS-1]);
 
 	for (iter=0; iter<NUMBER_INDIVIDUALS; iter++)
 	{
 		if (temp <= sig_fit_[iter])
 		{
-			nana = pop_[iter].individual();
+			nana = pop_[iter].gen;
 			break;
 		}
 	}
 }
 
-void population::splicer(RNG& rudi, GENO_TYPE& mama, GENO_TYPE& papa)
+void population::splicer(GENO_TYPE& mama, GENO_TYPE& papa)
 {
 	// Crossover loci (0 ~ 124)
 
@@ -283,14 +218,14 @@ void population::splicer(RNG& rudi, GENO_TYPE& mama, GENO_TYPE& papa)
 
 	for (iter=0; iter<CROSSOVER_POINTS; iter++)
 	{
-		if (rudi.uniform( 0, (int)(1/CROSSOVER_RATE)) < 1)
+		if (rudi_.uniform( 0, (int)(1/CROSSOVER_RATE)) < 1)
 		{
-			locus[iter] = rudi.uniform(0, NUMBER_GENES);
+			locus[iter] = rudi_.uniform(0, NUMBER_GENES);
 		}
 	}
 
 	std::vector<uint32_t> loci (locus, locus+CROSSOVER_POINTS);
-	std::sort (loci.begin(), loci.end(), lte_comp);
+	std::sort (loci.begin(), loci.end(), loci_comp);
 
 	std::vector<uint32_t>::iterator it;
 	it = std::unique(loci.begin(), loci.end());
@@ -319,14 +254,104 @@ void population::splicer(RNG& rudi, GENO_TYPE& mama, GENO_TYPE& papa)
 	papa = by;
 }
 
-void population::mutator(RNG& rudi)
+void population::mutator(void)
 {
 	uint32_t iter;
 	for (iter=0; iter<NUMBER_INDIVIDUALS; iter++)
 	{
-		pop_[iter].mutate(rudi);
+		pop_[iter] = mutate(pop_[iter]);
 	}
 }
+
+void population::calcFitness(specimen_t& indi)
+{
+	FITNESS_TYPE temp = 1.0;
+	FITNESS_TYPE L1 = 1.0;
+	FITNESS_TYPE L2 = 1.0;
+	FITNESS_TYPE L3 = 1.0;
+	uint32_t iter = 0, jter = 0;
+
+	// Positive Likelihood
+	for (iter=0; iter<NUMBER_SYMPTOMS; iter++)
+	{
+		if (SymptomSet[iter])
+		{
+			temp = 1.0;
+			for (jter=0; jter<NUMBER_GENES; jter++)
+			{
+				if (indi.gen[jter])
+				{
+					temp *= (1.0-qManifestationInDisease[iter][jter]);
+				}
+			}
+			L1 *= (1.0-temp);
+			if ((1.0-temp)>ZERO_FITNESS_LIMIT)
+			{
+				L1 *= (1.0-temp);
+			}
+			else
+			{
+				L1 *= ZERO_FITNESS_LIMIT;
+			}
+		}
+	}
+
+	// Negative Likelihood
+	for (iter=0; iter<NUMBER_GENES; iter++)
+	{
+		if (indi.gen[iter])
+		{
+			temp = 1.0;
+			for (jter=0; jter<NUMBER_SYMPTOMS; jter++)
+			{
+				if (SymptomSet[jter] == 0)
+				{
+					temp *= (1.0-qManifestationInDisease[jter][iter]);
+				}
+			}
+			L2 *= (temp);
+		}
+	}
+
+	// Prior Likelihood
+	for (iter=0; iter<NUMBER_GENES; iter++)
+	{
+		if (indi.gen[iter])
+		{
+			L3 *= qPriorLikelihood[iter];
+		}
+	}
+
+	indi.fit = (L1 * L2 * L3);
+}
+
+specimen_t population::populate(void)
+{
+	specimen_t indi;
+	indi.gen = rudi_.uniform(0, 2^(NUMBER_DIMENSIONS));
+	calcFitness(indi);
+	return indi;
+}
+
+specimen_t population::mutate(specimen_t indi)
+{
+	uint32_t iter;
+	for (iter=0; iter<NUMBER_GENES; iter++)
+	{
+		if (rudi_.uniform( 0, (int)(1/MUTATION_RATE) ) < 1)
+		{
+			indi.gen.flip(iter);
+		}
+	}
+	calcFitness(indi);
+	return indi;
+}
+
+
+
+
+
+
 
 
 
@@ -349,15 +374,15 @@ int main(void)
 		uint32_t trailer_trash;
 		for (trailer_trash=0; trailer_trash<NUMBER_TRIALS; trailer_trash++)
 		{
-			population hoponpop;
+			population hoponpop(randi);
 
-			hoponpop.populator(randi);
+			hoponpop.populator();
 
 
 			uint32_t generational_recursion;
 			for (generational_recursion=0; generational_recursion<NUMBER_GENERATIONS; generational_recursion++)
 			{
-				hoponpop.breeder(randi);
+				hoponpop.breeder();
 			}
 
 			// Print data to file
