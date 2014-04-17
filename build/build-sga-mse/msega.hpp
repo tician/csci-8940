0280038e189e808581ce192615746528811cdcdb
tician <tician@gmail.com>
2014-04-16 20:41:13 -0400

    started mse sga.  hopefully finish it soon.

---
+++ build/build-sga-mse/msega.hpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+//#include <bitset>
+
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+#include <boost/program_options.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+
+using namespace boost;
+namespace po = boost::program_options;
+using namespace std;
+
+
+#define NUMBER_TRIALS				10
+#define NUMBER_GENERATIONS			30000
+
+#define NUMBER_TRACKING				3
+#define NUMBER_GENES				7
+
+#define MAXIMIZING_FITNESS_VALUE
+
+#ifdef MAXIMIZING_FITNESS_VALUE
+#define HORRIFIC_FITNESS_VALUE		0.0
+#else
+#define HORRIFIC_FITNESS_VALUE		1.0e12
+#endif
+
+#define FITNESS_TYPE				double
+#define GENO_TYPE					uint64_t
+//#define GENO_TYPE					bitset<NUMBER_GENES>
+
+
